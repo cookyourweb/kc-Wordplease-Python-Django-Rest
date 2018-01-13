e125ce0cf4db8611f84283e224c813aec461151f
Veronica Serna <verserper@gmail.com>
2018-01-13 20:54:26 +0100

    Registramos nuestros modelos en el admin de django

--- src/articles/admin.py
+++ src/articles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from articles.models import Category, Articulo
 
 # Register your models here.
+admin.site.register(Category),
+admin.site.register(Articulo)
--- src/articles/models.py
+++ src/articles/models.py
@@ -6,6 +6,13 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)  # optional
 
+    def __str__(self):  # 0 parametros
+        """
+        Devuelve la representación de un objeto como una string
+        """
+        return self.namegit
+
+
 
 class Articulo(models.Model):
 
@@ -22,3 +29,13 @@ class Articulo(models.Model):
     modified_at = models.DateTimeField(auto_now=True)  # saves the date when the object is updated
 
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
