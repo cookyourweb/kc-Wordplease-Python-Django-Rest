f53c7adb05d51135f3ea8ce20e6844093530aed3
Veronica Serna <verserper@gmail.com>
2018-01-13 09:49:48 +0100

    añadido un helloWorld views urls

--- src/articles/views.py
+++ src/articles/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def hello_world(request):
+    name = request.GET.get("name")
+    if name is None:
+        return HttpResponse ("Hello keepcoders ")
+    else:
+        return HttpResponse ("Hello keepcoder: " + name)
+
